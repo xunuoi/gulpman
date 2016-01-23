8f7cceb965efad55dab0d108c979b7cb6942ab62
Cloud <xunuoi@163.com>
2016-01-23 10:33:10 +0800

    use cnpm

--- scripts/install.js
+++ scripts/install.js
@@ -1,1 +1,10 @@
-console.log('wow**************')
+
+var sh = require('shelljs')
+console.log('now install')
+
+sh.exec('npm install -g cnpm --registry=https://registry.npm.taobao.org')
+
+sh.exec('cnpm install gulp-sass --save')
+
+
+console.log('\nInstall Completed!\nGulpman Install Succeed!\nNow you can use the gulpman module in your gulpfile.js')
