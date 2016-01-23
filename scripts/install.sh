84e9f5605a94f998131e9d03d45275ebcf2f5bf8
Cloud <xunuoi@163.com>
2016-01-23 11:58:36 +0800

    installsh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+echo "\n\n*Check whether the gulp is installed in global:"
+
+gulp -v >& /dev/null
+
+if [ $? -eq 0 ];then
+    echo "\n*gulp has been installed"
+else
+    echo "\n*gulp not installed, now install gulp -g"
+    npm install gulp -g
+fi
+
+
 echo "\n\n*Check whether the cnpm is installed:"
 
 cnpm >& /dev/null
